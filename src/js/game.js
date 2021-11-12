0221ac4e43be569982433fcd7362b7451da85dfe
jtschuwirth <jtschuwirth@uc.cl>
2021-11-12 16:10:53 -0300

    arreglo de las files y comienzo codigo js

--- src/js/game.js
+++ src/js/game.js
@@ -1,9 +1,32 @@
-//npm run game
+//comando para compilar
+//npm run build 
+//para correr la pagina en localhost
+//npm run dev
 
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+//Clase Game clase superior donde se guardan los estados de la partida
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
 
+    render() {
+        return (
+            <div>
+                Aqui esta el juego
+            </div>
+        );
+    }
+}
+
+//clase canvas la cual tiene de hijos a los hexagonos, es el tablero en si
 class Canvas extends React.Component {
 
 }
+
+//comando necesario para importar la clase en main.js
+module.exports.Game = Game;
---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+
+const game = require("./game.js");
+const { ethers, Contract } = require("ethers");
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+//Funcion de inicializacion de la pagina
+const initialize = async () => {
+    ReactDOM.render(
+        <game.Game />,
+        document.getElementById('game')
+    );
+}
+
+window.addEventListener('DOMContentLoaded', initialize);
