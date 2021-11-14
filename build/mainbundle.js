15f646500eb55c7491bcb186e9f92192d6183907
jtschuwirth <jtschuwirth@uc.cl>
2021-11-14 20:52:35 -0300

    hexagon

--- build/mainbundle.js
+++ build/mainbundle.js
@@ -30493,7 +30493,14 @@ class Game extends React.Component {
                 React.createElement("div", {class: "center"}, 
                     "Turno: ", this.state.currentTurn
                 ), 
+                React.createElement("div", {class: "center"}, 
+                    "Tienes: ", this.state.currentPoints, " Puntos"
+                ), 
                 React.createElement("br", null), 
+
+                React.createElement("div", {class: "center"}, 
+                    React.createElement(Hexagon, null)
+                ), 
 
                 React.createElement("div", {class: "center"}, 
                     React.createElement(Board, {
@@ -30517,9 +30524,6 @@ class Game extends React.Component {
                     React.createElement("button", {onClick:  () => this.endTurn()}, "End Turn")
                 ), 
                 React.createElement("br", null), 
-                React.createElement("div", {class: "center"}, 
-                    "Tienes: ", this.state.currentPoints, " Puntos"
-                ), 
                 React.createElement("div", {class: "center"}, 
                     this.state.errorlog
                 ), 
@@ -30585,8 +30589,6 @@ class Game extends React.Component {
             }
         }
     }
-
-    
 
     dist(coord_1, coord_2) {
         return Math.abs(coord_1.x - coord_2.x) + Math.abs(coord_1.y - coord_2.y)
@@ -30707,6 +30709,64 @@ class Square extends React.Component {
         )
 
     }
+}
+
+class Hexagon extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            hexSize: 30,
+        }
+    }
+
+    componentWillMount() {
+        this.setState({
+            canvasSize: {canvasWidth: 800, canvasHeight: 600}
+        })
+    }
+
+    componentDidMount() {
+        const {canvasWidth, canvasHeight} = this.state.canvasSize;
+        this.canvashex.width = canvasWidth;
+        this.canvashex.height = canvasHeight;
+        this.drawHex(this.canvashex, {x: 30, y:30});
+    }
+    getHexCornerCord(center, i) {
+        let angle_deg = 60 * i -30;
+        let angle_rad = Math.PI / 180 * angle_deg;
+        let x = center.x +this.state.hexSize * Math.cos(angle_rad);
+        let y = center.y +this.state.hexSize * Math.sin(angle_rad);
+        return this.Point(x, y);
+
+    }
+
+    Point(x, y) {
+        return {x: x, y: y};
+    }
+
+    drawLine(canvasID, start, end) {
+        const ctx = canvasID.getContext("2d");
+        ctx.beginPath();
+        ctx.moveTo(start.x, start.y);
+        ctx.lineTo(end.x, end.y);
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+    drawHex(canvasID, center) {
+        for (let i = 0; i <= 5; i++) {
+            let start = this.getHexCornerCord(center, i);
+            let end = this.getHexCornerCord(center, i+1);
+            this.drawLine(canvasID, {x: start.x, y: start.y}, {x: end.x, y: end.y})
+        }
+    }
+     render() {
+         return (
+             React.createElement("div", null, 
+                 React.createElement("canvas", {ref: canvashex => this.canvashex = canvashex})
+             )
+         )
+     }
 }
 
 class Popup extends React.Component {
--- src/js/game.js
+++ src/js/game.js
@@ -178,7 +178,14 @@ class Game extends React.Component {
                 <div class="center">
                     Turno: {this.state.currentTurn}          
                 </div>
+                <div class="center">
+                    Tienes: {this.state.currentPoints} Puntos
+                </div>
                 <br></br>
+
+                <div class="center">
+                    <Hexagon />
+                </div>
 
                 <div class="center">
                     <Board 
@@ -202,9 +209,6 @@ class Game extends React.Component {
                     <button onClick={ () => this.endTurn()}>End Turn</button>
                 </div>
                 <br></br>
-                <div class="center">
-                    Tienes: {this.state.currentPoints} Puntos
-                </div>
                 <div class="center">
                     {this.state.errorlog}
                 </div>
@@ -270,8 +274,6 @@ class Game extends React.Component {
             }
         }
     }
-
-    
 
     dist(coord_1, coord_2) {
         return Math.abs(coord_1.x - coord_2.x) + Math.abs(coord_1.y - coord_2.y)
@@ -392,6 +394,64 @@ class Square extends React.Component {
         )
 
     }
+}
+
+class Hexagon extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            hexSize: 30,
+        }
+    }
+
+    componentWillMount() {
+        this.setState({
+            canvasSize: {canvasWidth: 800, canvasHeight: 600}
+        })
+    }
+
+    componentDidMount() {
+        const {canvasWidth, canvasHeight} = this.state.canvasSize;
+        this.canvashex.width = canvasWidth;
+        this.canvashex.height = canvasHeight;
+        this.drawHex(this.canvashex, {x: 30, y:30});
+    }
+    getHexCornerCord(center, i) {
+        let angle_deg = 60 * i -30;
+        let angle_rad = Math.PI / 180 * angle_deg;
+        let x = center.x +this.state.hexSize * Math.cos(angle_rad);
+        let y = center.y +this.state.hexSize * Math.sin(angle_rad);
+        return this.Point(x, y);
+
+    }
+
+    Point(x, y) {
+        return {x: x, y: y};
+    }
+
+    drawLine(canvasID, start, end) {
+        const ctx = canvasID.getContext("2d");
+        ctx.beginPath();
+        ctx.moveTo(start.x, start.y);
+        ctx.lineTo(end.x, end.y);
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+    drawHex(canvasID, center) {
+        for (let i = 0; i <= 5; i++) {
+            let start = this.getHexCornerCord(center, i);
+            let end = this.getHexCornerCord(center, i+1);
+            this.drawLine(canvasID, {x: start.x, y: start.y}, {x: end.x, y: end.y})
+        }
+    }
+     render() {
+         return (
+             <div>
+                 <canvas ref={canvashex => this.canvashex = canvashex }></canvas>
+             </div>
+         )
+     }
 }
 
 class Popup extends React.Component {
