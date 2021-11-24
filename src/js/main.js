edbca68d48ea831c81f1efa4cddd6f1215911fac
jtschuwirth <jtschuwirth@uc.cl>
2021-11-24 15:21:04 -0300

    testeo 2 cuentas

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,44 @@
 
+const { ethers, Contract } = require("ethers");
 const game = require("./game.js");
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+const signer = provider.getSigner();
+
+const cuenta_actual = async () => {
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    const account = accounts[0];
+    return account;
+}
 
 //Funcion de inicializacion de la pagina
-const initialize = () => {
+const initialize = async () => {
+    const MetaMaskClientCheck = () => {
+        const onboardButton = document.getElementById('connectButton');
+        //Now we check to see if MetaMask is installed
+        
+        if (ethereum.isMetaMask == true) {
+          onboardButton.innerText = 'Connect Metamask';
+          onboardButton.onclick = onClickConnect;
+          onboardButton.disabled = false;
+        }
+    }
+    const onClickConnect = async () => {
+        try {
+          // Will open the MetaMask UI
+          // You should disable this button while the request is pending!
+          await ethereum.request({ method: 'eth_requestAccounts' });
+        } catch (error) {
+          console.error(error);
+        }
+    }
+
+    MetaMaskClientCheck();
+    const address = await cuenta_actual();
     ReactDOM.render(
-        <game.PlayButton userAddress = {"Address"} />,
+        <game.PlayButton userAddress = {address} />,
         document.getElementById('playButton')
     );
 }
